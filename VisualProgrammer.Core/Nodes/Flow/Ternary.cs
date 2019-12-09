1be85930ecf1b72e4f881fa5564f0d091d295dbe
Will Bennion <will.bennion@outlook.com>
2019-12-09 19:06:11 +0000

    Added ternary node.

---
+++ VisualProgrammer.Core/Nodes/Flow/Ternary.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+
+namespace VisualProgrammer.Core.Nodes.Flow {
+
+	public class Ternary<T> : VisualExpression<T> {
+
+		[VisualNodeProperty(Order = -1)]
+		public ExpressionReference<bool> Condition { get; set; }
+
+
+		[VisualNodeProperty]
+		public ExpressionReference<T> A { get; set; }
+
+
+		[VisualNodeProperty]
+		public ExpressionReference<T> B { get; set; }
+
+		public override Expression CreateExpression(VisualProgram context) => Expression.Condition(
+			Condition.ResolveRequiredExpression(context),
+			A.ResolveRequiredExpression(context),
+			B.ResolveRequiredExpression(context)
+		);
+	}
+}

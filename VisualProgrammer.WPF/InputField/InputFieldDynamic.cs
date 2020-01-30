9618bf3ff62fd7d4a6a9e16929572b002932a801
Will Bennion <will.bennion@outlook.com>
2020-01-30 13:19:24 +0000

    WPF: Added functionality to the InputField to select the control based on the type. Numerical types are not yet supported. TODO: Add the ability for library consumers to be able to override and add new editor templates.

--- VisualProgrammer.WPF/InputField/InputFieldDynamic.cs
+++ VisualProgrammer.WPF/InputField/InputFieldDynamic.cs
@@ -10,6 +10,11 @@ namespace VisualProgrammer.WPF.InputField {
 			DefaultStyleKeyProperty.OverrideMetadata(typeof(InputFieldDynamic), new FrameworkPropertyMetadata(typeof(InputFieldDynamic)));
 		}
 
+		public override void OnApplyTemplate() {
+			base.OnApplyTemplate();
+			if (GetTemplateChild("PART_ContentControl") is ContentControl cc)
+				cc.DataContext = new InputFieldViewModel(this);
+		}
 
 		#region InputType DependencyProperty
 		public Type InputType {
@@ -28,7 +33,31 @@ namespace VisualProgrammer.WPF.InputField {
 		}
 
 		public static readonly DependencyProperty ValueProperty =
-			DependencyProperty.Register("Value", typeof(object), typeof(InputFieldDynamic), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender));
+			DependencyProperty.Register("Value", typeof(object), typeof(InputFieldDynamic), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, ValuePropertyChanged));
+
+		// Need to tell the view-model that the value has changed so that anything bound to that will update if the value is changed externally.
+		private static void ValuePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) => ((InputFieldDynamic)d).ValueChanged?.Invoke(d, EventArgs.Empty);
+		public event EventHandler ValueChanged;
 		#endregion
+	}
+
+
+	/// <summary>
+	/// DataTemplateSelector that tries to find a DataTemplate that is keyed with the type specified in the container's view-model's InputType.
+	/// </summary>
+	public class InputFieldDataTemplateSelector : DataTemplateSelector {
+
+		// Key to the fallback template to be displayed if an editor cannot be found
+		private static readonly ComponentResourceKey errorKey = new ComponentResourceKey(typeof(InputFieldDynamic), "MissingInputFieldDataTemplate");
+
+		public override DataTemplate SelectTemplate(object item, DependencyObject container) {
+			if (!(container is ContentPresenter presenter)) return null;
+			if (item is InputFieldViewModel vm) {
+				// First, try to find a template for this exact type
+				if (presenter.TryFindResource(new ComponentResourceKey(typeof(InputFieldDynamic), vm.InputType)) is DataTemplate dt)
+					return dt;
+			}
+			return presenter.FindResource(errorKey) as DataTemplate;
+		}
 	}
 }
